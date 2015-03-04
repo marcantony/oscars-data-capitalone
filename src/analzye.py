bfc96c15f93d121c5d42cef8e7827a5299759a48
Joshua Zeid <joshua.zeid@oasisdigital.com>
2015-03-04 12:51:06 -0600

    Corrected reassignment of keyword

--- src/analzye.py
+++ src/analzye.py
@@ -3,11 +3,11 @@ import csv, json
 def openResource(s):
     return open('../resources/' + s)
 
-file = openResource('oscar_tweets.csv')
+csvfile = openResource('oscar_tweets.csv')
 headers = json.load(openResource('headers.json'))
 best_picture = [line.rstrip('\n') for line in openResource('best_pictures.txt').readlines()]
 
-with file as f:
+with csvfile as f:
     reader = csv.DictReader(f)
     i = 0
     #for row in reader:

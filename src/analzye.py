4d740b8bfc1108e73fc5ef5be0cf00d9b2ebbe53
Joshua Zeid <joshua.zeid@oasisdigital.com>
2015-03-04 18:13:21 -0600

    Analyze text for each row to determine if it contains any form of a Best Picture nominee

--- src/analzye.py
+++ src/analzye.py
@@ -1,14 +1,28 @@
-import csv, json
+import csv
+import json
+
+from collections import defaultdict
 
 def openResource(s):
     return open('../resources/' + s)
 
 csvfile = openResource('oscar_tweets.csv')
 headers = json.load(openResource('headers.json'))
-best_picture = [line.rstrip('\n') for line in openResource('best_pictures.txt').readlines()]
+best_pictures = [line.rstrip('\n') for line in openResource('best_pictures.txt').readlines()]
 
 with csvfile as f:
     reader = csv.DictReader(f)
-    i = 0
-    #for row in reader:
-        # logic here
+    nomineeCount = defaultdict(int)
+    popularityRank = list()
+    for row in reader:
+
+        # find mentions of Best Picture nominees
+        text = row[headers['text']].lower();
+        for title in best_pictures:
+            t = title.lower()
+            if t in text or t.replace(' ', '') in text:
+                nomineeCount[title] += 1
+
+    popularityRank = sorted(nomineeCount, key=nomineeCount.get, reverse=True)
+    print nomineeCount
+    print popularityRank

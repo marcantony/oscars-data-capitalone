cecb80bb9df8d5afbc6f847e62079a003f75aefb
Joshua Zeid <joshua.zeid@oasisdigital.com>
2015-03-04 22:53:19 -0600

    Bucket tweets by checking location data for state name or abbreviation

--- src/analyze.py
+++ src/analyze.py
@@ -10,12 +10,15 @@ def openResource(s):
 csvfile = openResource('oscar_tweets.csv')
 headers = json.load(openResource('headers.json'))
 best_pictures = [line.rstrip('\n') for line in openResource('best_pictures.txt').readlines()]
+states = json.load(openResource('states.json'))
 
 with csvfile as f:
     reader = csv.DictReader(f)
     nomineeCount = defaultdict(int)
     timeCount = defaultdict(int)
+    stateCount = defaultdict(int)
     popularityRank = list()
+    stateRank = list()
     for row in reader:
 
         # find mentions of Best Picture nominees
@@ -31,11 +34,23 @@ with csvfile as f:
                         '%a %b %d %H:%M:%S +0000 %Y').replace(second=0)
                     timeCount[mention_time] += 1
 
+        # determine what state tweet is from, if data is available
+        location = row[headers['loc']]
+        for stateCode, stateNames in states.iteritems():
+            try:
+                if any(stateName in location for stateName in stateNames):
+                    stateCount[stateCode] += 1
+            except UnicodeDecodeError:
+                continue
+
     popularityRank = sorted(nomineeCount, key=nomineeCount.get, reverse=True)
     birdmanAnnounceTime = max(timeCount, key=timeCount.get)
+    stateRank = sorted(stateCount, key=stateCount.get, reverse=True)
 
     # naively convert to PST
     birdmanAnnounceTime += timedelta(hours=-8)
    
     print birdmanAnnounceTime.strftime('%I:%M %p')
     print popularityRank
+    print json.dumps(stateCount)
+    print stateRank
